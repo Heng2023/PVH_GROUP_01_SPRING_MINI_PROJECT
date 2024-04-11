47854144b09ef64dbd41cdf86f6787462f087560
soklaymeng <mengsoklay2222@gmail.com>
2024-04-11 23:39:36 +0700

    Map Categoryy

--- src/main/java/org/example/expensetracking/repository/CategoryRepository.java
+++ src/main/java/org/example/expensetracking/repository/CategoryRepository.java
@@ -1,7 +1,26 @@
 package org.example.expensetracking.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.example.expensetracking.model.Category;
+
+
+import java.util.List;
 
 @Mapper
 public interface CategoryRepository {
+    @Select("""
+              SELECT * FROM categories
+       LIMIT #{size}
+       OFFSET #{size} * (#{page} - 1)
+       """)
+    @Results(id = "categoryMapping", value = {
+            @Result(property = "categoryID",column = "category_id"),
+            @Result(property = "name", column = "name"),
+            @Result(property = "description", column = "description"),
+            @Result(property = "userId",column = "user_id")
+    })
+    List<Category> getAllCategory(Integer page, Integer size);
 }

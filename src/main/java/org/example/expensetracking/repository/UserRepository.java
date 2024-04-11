d8dc807ea8e533028c431e9de77826a56beee171
soklaymeng <mengsoklay2222@gmail.com>
2024-04-11 22:26:04 +0700

    Map all users in repostory

--- src/main/java/org/example/expensetracking/repository/UserRepository.java
+++ src/main/java/org/example/expensetracking/repository/UserRepository.java
@@ -1,7 +1,25 @@
 package org.example.expensetracking.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.example.expensetracking.model.User;
+
+import java.util.List;
 
 @Mapper
 public interface UserRepository {
+    @Select("""
+              SELECT * FROM users
+       LIMIT #{size}
+       OFFSET #{size} * (#{page} - 1)
+       """)
+    @Results (id = "userMapping", value = {
+            @Result(property = "userId",column = "user_id"),
+            @Result(property = "email", column = "email"),
+            @Result(property = "password", column = "password"),
+            @Result(property = "profileImage",column = "profile_image")
+    })
+    List<User> getAllUsers(Integer page, Integer size);
 }

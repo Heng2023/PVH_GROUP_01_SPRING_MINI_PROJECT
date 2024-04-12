9ef68245656953cf0d841121a48257030e29cc31
sreyly-long <longsreyly85@gmail.com>
2024-04-12 14:01:34 +0700

    insert category

--- src/main/java/org/example/expensetracking/repository/CategoryRepository.java
+++ src/main/java/org/example/expensetracking/repository/CategoryRepository.java
@@ -1,16 +1,15 @@
 package org.example.expensetracking.repository;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.example.expensetracking.model.Category;
+import org.example.expensetracking.model.dto.request.CategoryRequest;
 
 
 import java.util.List;
 
 @Mapper
 public interface CategoryRepository {
+    //Get All Category
     @Select("""
               SELECT * FROM categories
        LIMIT #{size}
@@ -23,4 +22,15 @@ public interface CategoryRepository {
             @Result(property = "userId",column = "user_id")
     })
     List<Category> getAllCategory(Integer page, Integer size);
+
+    //Post Category
+    @Select("""
+    INSERT INTO categories ( name, description, user_id ) VALUES
+    VALUES (#{name}, #{description},#{userId})
+    RETURNING *
+""")
+    @ResultMap("categoryMapping")
+    List<Category> insertCategory(CategoryRequest categoryRequest);
 }
+
+

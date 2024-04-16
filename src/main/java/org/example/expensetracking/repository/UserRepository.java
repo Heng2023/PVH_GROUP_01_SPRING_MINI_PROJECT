0b39004f79dd43e94c9427cdcb8acf5d6f09fed3
Sokheng Tey <sokheng.tey03@gmail.com>
2024-04-16 23:49:39 +0700

    forget-user-detail

--- src/main/java/org/example/expensetracking/repository/UserRepository.java
+++ src/main/java/org/example/expensetracking/repository/UserRepository.java
@@ -12,15 +12,25 @@ public interface UserRepository {
             @Result(property = "profileImage", column = "profile_image")
     })
     @Select("""
-        INSERT INTO users  VALUES (DEFAULT,#{user.email} , #{user.password},#{user.profileImage}) RETURNING user_id, *
+        INSERT INTO users  
+        VALUES (DEFAULT,#{user.email} , #{user.password},#{user.profileImage}) 
+        RETURNING user_id, *
     """)
     AppUserDTO saveUser(@Param("user") RegisterRequest registerRequest);
 
     @Select("""
-        SELECT * FROM users WHERE email = #{email}
+        SELECT * 
+        FROM users 
+        WHERE email = #{email}
     """)
     User findUserByEmail(@Param("email") String email);
 
-    @Update("UPDATE users SET password = #{encodedPassword} WHERE email = #{email}")
-    void updatePasswordByEmail(@Param("email") String email, @Param("encodedPassword") String encodedPassword);
+    @ResultMap("UserMapping")
+    @Select("""
+        UPDATE users 
+        SET password = #{encodedPassword} 
+        WHERE email = #{email}
+        RETURNING user_id, email,password, profile_image
+    """)
+    AppUserDTO updatePasswordByEmail(@Param("email") String email, @Param("encodedPassword") String encodedPassword);
 }

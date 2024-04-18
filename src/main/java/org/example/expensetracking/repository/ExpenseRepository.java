99ec2cf69a9acc706209db78c5ff6fc10c57d504
Panha <chhelpanha2@gmail.com>
2024-04-18 20:29:42 +0700

    remove userId param on save expense method mapping

--- src/main/java/org/example/expensetracking/repository/ExpenseRepository.java
+++ src/main/java/org/example/expensetracking/repository/ExpenseRepository.java
@@ -4,16 +4,23 @@ import org.apache.ibatis.annotations.*;
 import org.example.expensetracking.model.Expense;
 import org.example.expensetracking.model.dto.request.ExpenseRequest;
 
+
 @Mapper
 public interface ExpenseRepository {
 
     @Select("""
     INSERT INTO categories (name, description, user_id, category_id)
-    VALUES (#{name}, #{description}, #{userId}, #{categoryId})
+    VALUES (#{expense.name}, #{expense.description}, #{userId}, #{categoryId})
     RETURNING *
     """)
     @Results(id = "expenseMapping", value = {
-            @Result()
+            @Result(property = "expenseId", column = "expense_id"),
+
+            @Result(property = "user", column = "user_id",
+            one = @One(select = "org.example.expensetracking.repository.UserRepository.findUserById")),
+
+            @Result(property = "category", column = "category_id",
+            one = @One(select = "org.example.expensetracking.repository.CategoryRepository.findCategoryById"))
     })
     Expense saveExpense(@Param("expense") ExpenseRequest expenseRequest);
 }

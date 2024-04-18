cbb4696ddaa303c6a0372ed21261c807400e8f6a
Panha <chhelpanha2@gmail.com>
2024-04-18 20:06:38 +0700

    add save expense mapping

--- src/main/java/org/example/expensetracking/repository/ExpenseRepository.java
+++ src/main/java/org/example/expensetracking/repository/ExpenseRepository.java
@@ -1,7 +1,19 @@
 package org.example.expensetracking.repository;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
+import org.example.expensetracking.model.Expense;
+import org.example.expensetracking.model.dto.request.ExpenseRequest;
 
 @Mapper
 public interface ExpenseRepository {
+
+    @Select("""
+    INSERT INTO categories (name, description, user_id, category_id)
+    VALUES (#{name}, #{description}, #{userId}, #{categoryId})
+    RETURNING *
+    """)
+    @Results(id = "expenseMapping", value = {
+            @Result()
+    })
+    Expense saveExpense(@Param("expense") ExpenseRequest expenseRequest);
 }

57be0bee9abe25c50d3c3916241c2f3b3c0abb9b
Panha <chhelpanha2@gmail.com>
2024-04-18 20:06:14 +0700

    add save expense endpoint

--- src/main/java/org/example/expensetracking/controller/ExpenseController.java
+++ src/main/java/org/example/expensetracking/controller/ExpenseController.java
@@ -1,9 +1,20 @@
 package org.example.expensetracking.controller;
 
+import org.example.expensetracking.model.Expense;
+import org.example.expensetracking.model.dto.request.ExpenseRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/expense/")
 public class ExpenseController {
+
+    @PostMapping
+    public ResponseEntity<?> addExpense(@RequestBody ExpenseRequest expenseRequest) {
+
+        return null;
+    }
 }
